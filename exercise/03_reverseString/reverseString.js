49985197ce8d9c25a1d1fe3f7d2faff8a0374af2
Peter Choi <jy.peter.c@gmail.com>
2022-11-16 13:25:42 -0600

    completed exercise

--- exercise/03_reverseString/reverseString.js
+++ exercise/03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = "";
+    for (let i = 0; i < string.length; i++){
+        reversed = reversed.concat(string.slice(string.length-i-1,string.length-i));
+    }
+    return reversed;
 };
 
 // Do not edit below this line
