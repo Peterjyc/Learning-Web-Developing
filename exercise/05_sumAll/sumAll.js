728b171fb6323564b0f0304b0a3b4b9e6a6196a5
Peter Choi <jy.peter.c@gmail.com>
2022-11-17 22:20:16 -0600

    completed exercise

--- exercise/05_sumAll/sumAll.js
+++ exercise/05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let temp = 0;
+    let low = Math.min(start, end)
+    let high = Math.max(start,end)
+    if (low < 0 || isNaN(start) || isNaN(end)){
+        return 'ERROR';
+    }
+    if (typeof start === "number" && typeof end === "number"){
+        for (let i = low; i <= high; i++){
+            temp += i;
+        }
+        return temp;
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line
