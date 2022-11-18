fe6456247774457039437cbfe534f48689c719e7
Peter Choi <jy.peter.c@gmail.com>
2022-11-17 21:54:11 -0600

    completed exercise

--- exercise/04_removeFromArray/removeFromArray.js
+++ exercise/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...numbers) {
+    let resultArray = array;
+    temp = 0;
+    for (const num of numbers){
+        resultArray = resultArray.filter(number => number !== num);
+    }
+    return resultArray;
 };
 
 // Do not edit below this line
