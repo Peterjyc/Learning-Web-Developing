004549791764abc91e772cb5d202dcad06afed6a
Peter Choi <jy.peter.c@gmail.com>
2022-11-20 00:39:56 -0600

    save

--- Web Developing/Learning-Web-Developing/Odin/DOM_manipulation/script.js
+++ Web Developing/Learning-Web-Developing/Odin/DOM_manipulation/script.js
@@ -1,8 +1,19 @@
 const container = document.querySelector('#container');
 
 const content = document.createElement('div');
-const contentp = document.createElement('p');
 content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
-
 container.appendChild(content);
+
+const contentp = document.createElement('p');
+contentp.classList.add('contentp');
+contentp.textContent = "Hey I'm red!";
+contentp.style.color = "red";
+container.appendChild(contentp);
+
+const contenth3 = document.createElement('h3');
+contenth3.textContent = "I'm a blue h3!";
+contenth3.style.color = "blue";
+container.appendChild(contenth3);
+
+const contentdiv = document.createElement('div');
