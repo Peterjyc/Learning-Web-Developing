8e9aba0875e7ea2f8a186ee2d01ce94ecb1b2eb6
Peter Choi <jy.peter.c@gmail.com>
2022-12-11 15:32:49 -0600

    exercise completed

---
+++ Web Developing/Learning-Web-Developing/Odin/Fundamentals Pt5/Hello, object/script.js
@@ -0,0 +1,11 @@
+let user = {
+    name: "John",
+    surname: "Smith",
+}
+console.log(user.name, user.surname);
+
+user.name = "Pete"
+console.log(user.name, user.surname);
+
+delete user.name
+console.log(user.name, user.surname);
