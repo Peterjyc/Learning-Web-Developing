229c783d54bd522bcc1d0a77921d8774cf748f72
Peter Choi <jy.peter.c@gmail.com>
2022-12-12 17:22:22 -0600

    completed palindrome exercise

--- exercise/10_fibonacci/fibonacci.js
+++ exercise/10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0){
+        return "OOPS";
+    }
+    let fibArray = [1];
+    for (let i = 1; i <= num; i++){
+        if (i === 1){
+            fibArray.push(i);
+        } else {
+            let temp = fibArray[i-1]+fibArray[i-2];
+            fibArray.push(temp);
+        }
 
+    }
+    return fibArray[num-1];
 };
 
 // Do not edit below this line
