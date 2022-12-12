5efb664f87f6ea18ade993c16b10f54627122539
Peter Choi <jy.peter.c@gmail.com>
2022-12-12 16:59:01 -0600

    completed exercise

--- exercise/09_palindromes/palindromes.js
+++ exercise/09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let temp = string.replace(/[^a-zA-Z]/g, "");
+    let reversed = temp.split("").reverse().join("");
+    if (temp.toLowerCase() === reversed.toLowerCase()){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
