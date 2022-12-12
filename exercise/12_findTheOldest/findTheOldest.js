e4de7fbe2acd1297374c7237f93a4f3040cb209e
Peter Choi <jy.peter.c@gmail.com>
2022-12-12 17:54:14 -0600

    completed findTheOldest exercise

--- exercise/12_findTheOldest/findTheOldest.js
+++ exercise/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let temp = 0;
+    let age = 0;
+    let oldestPerson = "";
+    people.forEach(person => {
 
+        const currentYear = new Date().getFullYear();
+        if ('yearOfDeath' in person){
+            age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            age = currentYear - person.yearOfBirth
+        }
+        if (age > temp){
+            temp = age;
+            oldestPerson = person;
+        }
+    })
+    return oldestPerson;
 };
 
 // Do not edit below this line
