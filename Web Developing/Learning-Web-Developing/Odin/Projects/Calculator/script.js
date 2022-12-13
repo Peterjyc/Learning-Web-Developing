cf6169072878ef4a9e261923684bd5775f524804
Peter Choi <jy.peter.c@gmail.com>
2022-12-13 17:26:35 -0600

    working on calculator operations

--- Web Developing/Learning-Web-Developing/Odin/Projects/Calculator/script.js
+++ Web Developing/Learning-Web-Developing/Odin/Projects/Calculator/script.js
@@ -1,3 +1,4 @@
+//basic functions
 function add(num1, num2){
     return num1 + num2;
 }
@@ -14,6 +15,7 @@ function divide(num1,num2){
     return num1 / num2;
 }
 
+//operating function
 function operate(num1, num2, operator){
     if (operator === "+"){
         return add(num1, num2);
@@ -24,4 +26,30 @@ function operate(num1, num2, operator){
     }else if (operator === "/"){
         return divide(num1, num2);
     }
+}
+//loop through buttons to add corresponding function
+const buttons = document.querySelectorAll(".button");
+const screen = document.querySelector(".screen");
+
+let firstNum = 0;
+let secondNum = 0;
+let next = false;
+let operator = ""
+for (const button of buttons){
+    button.addEventListener("click", function() {
+        if (this.classList.contains("num") && next === false) {
+            if (screen.innerHTML === "0") {
+                // Replace the leading zero with the new digit
+                screen.innerHTML = this.id;
+            } else {
+                // Concatenate the new digit to the current input
+                screen.innerHTML += this.id;
+            }
+        }
+        if (this.classList.contains("op")) {
+            next = true;
+            firstNum = document.getElementById('screen').innerHTML;
+            operator = this.id;
+        }
+    });
 }
