5c956b95159d168234b1029c509f0c5eaa07d427
Peter Choi <jy.peter.c@gmail.com>
2022-12-14 23:55:38 -0600

    calculator project finished, all buttons operational

--- Web Developing/Learning-Web-Developing/Odin/Projects/Calculator/script.js
+++ Web Developing/Learning-Web-Developing/Odin/Projects/Calculator/script.js
@@ -1,6 +1,6 @@
 //basic functions
 function add(num1, num2){
-    return num1 + num2;
+    return (num1 + num2);
 }
 
 function subtract(num1, num2){
@@ -16,14 +16,15 @@ function divide(num1,num2){
 }
 
 //operating function
-function operate(num1, num2, operator){
-    if (operator === "+"){
+function operate(num1, num2, op){
+    switch (op) {
+    case "+":
         return add(num1, num2);
-    }else if (operator === "-"){
+    case "-":
         return subtract(num1, num2);
-    }else if (operator === "*"){
+    case "*":
         return multiply(num1, num2);
-    }else if (operator === "/"){
+    case "/":
         return divide(num1, num2);
     }
 }
@@ -31,25 +32,82 @@ function operate(num1, num2, operator){
 const buttons = document.querySelectorAll(".button");
 const screen = document.querySelector(".screen");
 
+let temp = 0;
 let firstNum = 0;
 let secondNum = 0;
 let next = false;
 let operator = ""
+let opExists = false;
+let result = 0;
 for (const button of buttons){
     button.addEventListener("click", function() {
         if (this.classList.contains("num") && next === false) {
             if (screen.innerHTML === "0") {
                 // Replace the leading zero with the new digit
                 screen.innerHTML = this.id;
+                temp = document.getElementById('screen').innerHTML;
             } else {
                 // Concatenate the new digit to the current input
                 screen.innerHTML += this.id;
+                temp = document.getElementById('screen').innerHTML;
             }
         }
+        //clear the screen for next number
+        if (this.classList.contains("num") && next === true) {
+            next = false;
+            screen.innerHTML = this.id;
+            temp = document.getElementById('screen').innerHTML;
+        }
+
+        //store operator
         if (this.classList.contains("op")) {
             next = true;
-            firstNum = document.getElementById('screen').innerHTML;
-            operator = this.id;
+            if (opExists === false){
+                opExists = true;
+                firstNum = parseFloat(temp);
+                operator = this.id;
+            }else if (opExists === true){
+                //evaluate operation
+                secondNum = parseFloat(temp);
+                result = operate(firstNum,secondNum,operator)
+                firstNum = result;
+                screen.innerHTML = firstNum;
+                operator = this.id;
+            }
+
+        }else{
+            next = false;
         }
+        if (this.classList.contains("eval")) {
+            secondNum = parseFloat(temp);
+            result = operate(firstNum,secondNum,operator)
+            firstNum = result;
+            temp = firstNum;
+            screen.innerHTML = firstNum;
+            opExists = false;
+        }
+        if (this.classList.contains("reset")) {
+            screen.innerHTML = 0;
+            firstNum = 0;
+            secondNum = 0;
+            operator = "";
+            opExists = false;
+            temp = 0;
+        }
+        if (this.classList.contains("inv")) {
+            let negativeNum = temp * -1;
+            screen.innerHTML = negativeNum;
+            temp = negativeNum;
+        }
+        if (this.classList.contains("percent")){
+            secondNum = parseFloat(temp);
+            result = operate(firstNum,secondNum,operator)
+            firstNum = result;
+            temp = firstNum;
+            screen.innerHTML = firstNum * 100;
+            screen.innerHTML += "%";
+            opExists = false;
+        }
+        console.log("Temp " + temp + ", first " + firstNum + ", second " + secondNum);
     });
 }
