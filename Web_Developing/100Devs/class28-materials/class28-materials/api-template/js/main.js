88e90daf6e1de7df9b0b9a0d44f2fa2b806c3a5e
Peter Choi <jy.peter.c@gmail.com>
2023-02-10 12:10:49 -0600

    js api practice

--- Web_Developing/100Devs/class28-materials/class28-materials/api-template/js/main.js
+++ Web_Developing/100Devs/class28-materials/class28-materials/api-template/js/main.js
@@ -1,27 +1,22 @@
-//Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
-  console.log(choice)
-  const url = `https://api.nasa.gov/planetary/apod?api_key=iJy0Nd4wZZzfW2HFgBkNV4DDUsZ6PxL3RqQRgFfI&date=${choice}`
+  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
-        if(data.media_type === "image"){
-          document.querySelector('img').src = data.hdurl
-        }else if(data.media_type === 'video'){
-          document.querySelector('iframe').src = data.url
-        }else{
-          alert('Media Not Supported - Contact NASA IMMEDIATLY')
-        }
-       
-        document.querySelector('h3').innerText = data.explanation
+        console.log(data.subclasses);
+        data.subclasses.forEach(element => {
+          console.log(element.name)
+          const li = document.createElement('li');
+          li.textContent = element.name
+          document.querySelector('.ul').appendChild(li);
+        })
+
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
-
