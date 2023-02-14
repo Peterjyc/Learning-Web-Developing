9faea4a55ed9fcc1104d6785189735a1db591a0a
Peter Choi <jy.peter.c@gmail.com>
2023-02-13 20:42:07 -0600

    completed exercise

--- Web_Developing/Learning-Web-Developing/Odin/webpack-demo/src/index.js
+++ Web_Developing/Learning-Web-Developing/Odin/webpack-demo/src/index.js
@@ -1,11 +1,14 @@
 import _ from 'lodash';
 
+import myName from './myName';
+
 function component() {
     const element = document.createElement('div');
 
     // Lodash, currently included via a script, is required for this line to work
     // Lodash is not imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.textContent = myName('Cody');
 
     return element;
   }
