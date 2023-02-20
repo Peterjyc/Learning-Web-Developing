47e5e6b683644a10da68483c4c1330de199c83d9
Peter Choi <jy.peter.c@gmail.com>
2023-02-20 14:00:58 -0600

    class practice

--- Web_Developing/100Devs/class31-materials/class31-materials/objects-child-class/js/main.js
+++ Web_Developing/100Devs/class31-materials/class31-materials/objects-child-class/js/main.js
@@ -1,19 +1,41 @@
-//Create an a class and extend it - Can be anything you would like it to be!
-
 class Animal {
     constructor(name){
-        this.name = name;
+        this._name = name;
+    }
+    get name() {
+        return this._name;
     }
     speak(){
-        console.log(`${this.name} makes a sound`)
+        console.log(`${this._name} makes a sound`)
     }
 }
 
-class Dog extends Animal {
-    constructor(name, breed) {
-        super(name)
-        this.breed = breed;
-    }
-}
 
-let simba = new Dog ('Simba', 'Sheperd');
+
+// //Create an a class and extend it - Can be anything you would like it to be!
+
+// class Animal {
+//     constructor(name){
+//         this.name = name;
+//     }
+//     speak(){
+//         console.log(`${this.name} makes a sound`)
+//     }
+// }
+
+// class Dog extends Animal {
+//     constructor(name, breed) {
+//         super(name)
+//         this.breed = breed;
+//     }
+// }
+
+// class Cat extends Animal {
+//     constructor(name, breed){
+//         super(name)
+//         this.breed = breed;
+//     }
+//     speak(){
+//         console.log('MEOW')
+//     }
+// }
