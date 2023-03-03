2a910d6cd42f9d72bbb5f0b38a973454fd128408
Peter Choi <jy.peter.c@gmail.com>
2023-03-02 19:31:41 -0600

    practice using api

--- Web_Developing/100Devs/class38-materials/class38-materials/rap-names-api/server.js
+++ Web_Developing/100Devs/class38-materials/class38-materials/rap-names-api/server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 
+const savage = {
+    'age' : 20,
+    'birthName': 'birthName',
+    'birthLocation': 'birthLocation'
+}
+
 app.get('/', (request, response) => {
     response.sendFile(__dirname  + '/index.html')
 })
 
 app.get('/api', (request, response)=>{
-    response.json()
+    response.json(savage)
 })
 
 app.listen(PORT, ()=>{
