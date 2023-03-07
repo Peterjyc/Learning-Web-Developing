71505fc577f940c5e7282f309067694e08c06318
Peter Choi <jy.peter.c@gmail.com>
2023-03-06 19:57:04 -0600

    mongodb practice

--- Web_Developing/100Devs/MongoDB/server.js
+++ Web_Developing/100Devs/MongoDB/server.js
@@ -5,23 +5,58 @@ const app = express();
 const MongoClient = require ('mongodb').MongoClient
 const connectionString = 'mongodb+srv://jypeterc:Peter359.@cluster0.dsbrdb2.mongodb.net/?retryWrites=true&w=majority'
 
-app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(3000, function(){
-    console.log('listening on 3000')
-})
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
-
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-})
-
-MongoClient.connect(connectionString, {
-    useUnifiedTopology: true
-  }, (err, client) => {
-    if (err) return console.error(err);
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
     console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+
+    app.set('view engine', 'ejs');
+
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(express.static('public'))
+    app.use(bodyParser.json())
+
+    app.listen(3000, function(){
+        console.log('listening on 3000')
+    })
+
+    app.get('/', (req, res) => {
+        db.collection('quotes').find().toArray()
+        .then(results => {
+            res.render('index.ejs', { quotes: results })
+        })
+        .catch(error => console.log(error))
+        console.log(req.body)
+    })
+
+    app.post('/quotes', (req, res) => {
+        console.log(req.body)
+        quotesCollection.insertOne(req.body)
+            .then(result => {
+                res.redirect('/')
+            })
+            .catch(err => console.error(error))
+    })
+
+    app.put('/quotes', (req, res) => {
+        quotesCollection.findOneAndUpdate(
+            { name: 'Yoda' },
+            {
+              $set: {
+                name: req.body.name,
+                quote: req.body.quote
+              }
+            },
+            {
+              upsert: true
+            }
+          )
+          .then(res => {
+            res.json('Success')
+          })
+          .catch(error => console.error(error))
+      })
+
   })
