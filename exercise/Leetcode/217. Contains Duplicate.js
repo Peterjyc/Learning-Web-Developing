a9d63d0d6c6df1847a5913771a26af07bbf48b89
Peter Choi <jy.peter.c@gmail.com>
2023-04-24 19:50:57 -0500

    completed exercise - changed solution

--- exercise/Leetcode/217. Contains Duplicate.js
+++ exercise/Leetcode/217. Contains Duplicate.js
@@ -3,11 +3,13 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let result = true;
+    let result = false;
+    let array = [];
     for (let i = 0; i < nums.length; i++){
-        if (nums.indexOf(nums[i]) !== nums.lastIndexOf(nums[i])){
-            result = false;
+        if (array.includes(nums[i])){
+            result = true;
         }
+        array.push(nums[i])
     }
     return result;
 };
