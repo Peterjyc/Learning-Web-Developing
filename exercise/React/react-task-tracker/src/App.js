c30f9234179cf361d6bc5b4a58bcc4a904452feb
Peter Choi <jy.peter.c@gmail.com>
2023-04-27 20:34:21 -0500

    save

--- exercise/React/react-task-tracker/src/App.js
+++ exercise/React/react-task-tracker/src/App.js
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 
  function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState(
     [
         {
@@ -19,6 +21,14 @@ import Tasks from './components/Tasks'
             reminder: true,
         }
     ])
+
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   //Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id ))
@@ -31,7 +41,8 @@ import Tasks from './components/Tasks'
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
 
     </div>
