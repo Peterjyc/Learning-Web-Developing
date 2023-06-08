9b61e22c8e81273f6e8921f856c48c47ec845117
Peter Choi <jy.peter.c@gmail.com>
2023-06-08 18:01:41 -0500

    update files

---
+++ Website/Peach/Development/peach-website/peach-website/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Navbar from './Navbar.js';
+import Main from './Main.js'
+import  Menu from './Menu.js'
+import Flavour from './Flavour.js'
+import Footer from './Footer.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Main />
+      <Menu />
+      <Flavour />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Website/Peach/Development/peach-website/peach-website/src/Flavour.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Flavour() {
+    return (
+        <div>
+            Flavour
+        </div>
+    )
+}
