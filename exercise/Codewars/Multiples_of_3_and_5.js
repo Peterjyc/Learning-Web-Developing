39bb29dfcb34cfd462c0f0670600e0fae4bb5468
Peter Choi <jy.peter.c@gmail.com>
2023-06-23 19:01:30 -0500

    completed exercise

--- exercise/Codewars/Multiples_of_3_and_5.js
+++ exercise/Codewars/Multiples_of_3_and_5.js
@@ -8,3 +8,16 @@ function solution(number){
   }
   return array.reduce((a,c) => a + c)
 }
+
+//re-do
+function solution(number){
+  //ex)number = 15; 3x (3,6,9,12,15), 5x (5,10,15)
+  //result = 3+6+9+12+15+5+10 = 60
+  let sum = 0;
+  for (let i = 0; i < number; i++){
+    if(i % 3 === 0 || i % 5 === 0){
+      sum += i;
+    }
+  }
+  return sum;
+}
