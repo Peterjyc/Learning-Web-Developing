9d894dade67b2b2c8ca76f6279b1f60a76c7ad19
Peter Choi <105335267+Peterjyc@users.noreply.github.com>
2023-06-28 09:56:04 -0500

    completed exercise

---
+++ exercise/Codewars/Which are in?.js
@@ -0,0 +1,8 @@
+function inArray(array1,array2){
+  const result = new Set()
+  for (let i = 0; i < array2.length; i++){
+   let temp = (array1.filter(el => array2[i].includes(el)))
+   temp[0] ? result.add(temp[0]) : null
+  }
+  return Array.from(result).sort()
+}
