29661a1391357aa63981d4d5ab006df69016619f
Peter Choi <jy.peter.c@gmail.com>
2023-07-07 11:49:13 -0500

    completed exercise

--- exercise/Codewars/Sum Mixed Array.js
+++ exercise/Codewars/Sum Mixed Array.js
@@ -5,3 +5,10 @@ function sumMix(x){
     })
     return sum
   }
+
+  function sumMix(x){
+    return x.map(el => {
+      return typeof(el) === 'string' ? Number(el) : el
+    })
+      .reduce((a,c) => a+c)
+  }
